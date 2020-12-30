fd2edd2b93a3a34b8ff631c2a93ea43fae0561c7
Shomari Roberts <shomariroberts@gmail.com>
2020-12-30 01:09:57 -0500

    added trigger event callbacks method

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { User } from './models/User';
 
 const user = new User({});
 
-user.on('change', () => {});
+user.on('change', () => {
+  console.log('change 1');
+});
 
-console.log(user);
+user.on('change', () => {
+  console.log('change 2');
+});
+
+user.trigger('change');
