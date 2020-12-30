fbcd666da798809c3b2d5b2ce58ade6c344178b2
Shomari Roberts <shomariroberts@gmail.com>
2020-12-30 00:46:20 -0500

    created User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi there');
+import { User } from './models/User';
+
+const user = new User({});
+
+user.set({ name: 'name' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
