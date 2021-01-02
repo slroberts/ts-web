8180dfe9c19c9deb754a196cb3220ca66908c693
Shomari Roberts <shomariroberts@gmail.com>
2021-01-02 16:13:35 -0500

    created Sync class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 interface UserProps {
   id?: number;
   name?: string;
@@ -6,6 +6,8 @@ interface UserProps {
 }
 
 export class User {
+  public events: Eventing = new Eventing();
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -14,22 +16,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
   }
 }
