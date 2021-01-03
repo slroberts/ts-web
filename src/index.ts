864a1e038828313b5a8225f5e4918558544c0fd1
Shomari Roberts <shomariroberts@gmail.com>
2021-01-03 10:52:39 -0500

    re-implemented to get, on and trigger methods

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'testw Name', age: 34 });
-
-user.events.on('change', () => {
-  console.log('change');
-});
-
-user.events.trigger('change');
