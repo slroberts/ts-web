0244d2b4322acd2401e8241b4bba5f80e566d0b3
Shomari Roberts <shomariroberts@gmail.com>
2021-01-03 18:35:33 -0500

    added model properties

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { UserForm } from './Views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'Name', age: 20 });
+
+const userForm = new UserForm(document.getElementById('root'), user);
 
 userForm.render();
