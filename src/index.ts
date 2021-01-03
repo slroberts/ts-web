ed2e970a2d49ac5f47a3d92fc426ef980e6b192a
Shomari Roberts <shomariroberts@gmail.com>
2021-01-03 11:06:29 -0500

    impleted the set method in the User class

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import { User } from './models/User';
 const user = new User({ name: 'testw Name', age: 34 });
 
 console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log('User was changed');
+});
+
+user.set({ name: 'newName' });
